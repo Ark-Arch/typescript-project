47ad0c81eec069f5945c3e876824c9efed1b76c3
Ark-Arch <69996990+Ark-Arch@users.noreply.github.com>
2025-04-12 12:02:55 +0000

    implementing interfaces to provide structures to classes

---
+++ dist/second.js
@@ -0,0 +1,17 @@
+"use strict";
+// i can use interfaces to implement structure in classes 
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burst, short) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+}
