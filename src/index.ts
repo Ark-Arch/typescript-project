80bf8701bc9c47926012f8189b77c4af17a72f07
Ark-Arch <69996990+Ark-Arch@users.noreply.github.com>
2025-04-12 09:13:37 +0000

    implementing setters and getters

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,35 @@
 
 //using modifiers instead: production ready code
 class User {
+
+    private _courseCount = 1
+
     readonly city: string= "Lagos State"
     constructor(
         public email: string,
         public name: string,
         // private userId: string
     ){
+    }
 
+    // private method
+    private deleteToken(){
+        console.log("Token deleted")
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum<=1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
     }
 }
 
